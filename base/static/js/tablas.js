bfec28243ea5c47c07845ead2f1b8ebd2c26e31e
Sebastian <jvelandiavelandia677@gmail.com>
2023-08-21 19:54:55 -0500

    Botones exportar

--- base/static/js/tablas.js
+++ base/static/js/tablas.js
@@ -1,14 +1,2 @@
-const table = $('#example').DataTable({
-    
-    scrollY: '200px'
-}); 
-document.querySelectorAll('a.toggle-vis').forEach((el) => {
-    el.addEventListener('click', function (e) {
-        e.preventDefault();
+const table = $('#example'); 
  
-        let columnIdx = e.target.getAttribute('data-column');
-        let column = table.column(columnIdx);
-
-        column.visible(!column.visible());
-    });
-});
